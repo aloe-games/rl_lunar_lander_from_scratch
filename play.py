5dbde0dfc02521ca97882044672be12e8cffc526
Marek Michalik <marekmichalik43@gmail.com>
2024-04-27 17:10:56 +0200

    Run on CPU

--- play.py
+++ play.py
@@ -7,7 +7,7 @@ env = gym.make("LunarLander-v2", render_mode="human")
 
 
 agent = QNetwork(8, 4)
-agent.load_state_dict(torch.load("model"))
+agent.load_state_dict(torch.load("model", map_location=torch.device('cpu')))
 
 episodes = 10
 total_reward = 0.0
