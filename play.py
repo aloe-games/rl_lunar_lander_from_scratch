09fbc88d0ac95f8b996ef8ab95fe1bbd428bf17a
Marek Michalik <marekmichalik43@gmail.com>
2025-01-31 02:31:24 +0100

    Quantize model even more

--- play.py
+++ play.py
@@ -1,21 +1,18 @@
-import pickle
-
 import gymnasium as gym
 import numpy as np
 
 env = gym.make("LunarLander-v2", render_mode="human")
 
-
-model = pickle.load(open("model.pickle", "rb"))
-weight = model["network.weight"]
-bias = model["network.bias"]
+weights = np.array([[-0.4, -0.3, 1.1, 10.0, 1.1, 0.0, 2.4, 2.7], [5.0, 1.1, 3.4, 3.3, -7.5, -3.3, -2.2, -3.3],
+                    [0.4, -2.0, -0.4, -7.1, -0.8, -1.9, 0.8, -0.3], [-5.1, 1.7, -4.2, 1.8, 8.2, 4.9, -4.5, -1.3], ])
+bias = np.array([-0.7, 0.9, -0.3, 0.1])
 
 episodes = 10
 total_reward = 0.0
 for i in range(episodes):
     observation, _ = env.reset()
     while True:
-        action = np.argmax(np.matmul(observation, weight.T) + bias)
+        action = np.argmax(np.matmul(observation, weights.T) + bias)
         observation, reward, terminated, truncated, _ = env.step(action)
 
         total_reward += reward
